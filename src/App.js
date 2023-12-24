8e37621f6837b95b820614182f54df6a11ef7a39
Mikechavo <mchavez76@yahoo.com>
2023-12-24 02:22:23 -0600

    pages/components and layout set up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from "react-router-dom"
 
-function App() {
+const App = () => {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/routineindex" element={<RoutineIndex />} />
+      <Route path="/routineshow" element={<RoutineShow />} />
+      <Route path="/routinenew" element={<RoutineNew />} />
+      <Route path="/routineedit" element={<RoutineEdit />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+    <Footer />
+  </>
   );
 }
 
---
+++ src/mockRoutines.js
@@ -0,0 +1,34 @@
+const mockRoutines = [
+  {
+    id: 1,
+    muscle_group: 'Tricep Pull Downs',
+    sets: 4,
+    reps: 15,
+    weight: 180,
+    image: "https://cdn5.vectorstock.com/i/1000x1000/49/69/man-doing-cable-tricep-pull-down-exercise-vector-35544969.jpg",
+    notes: 'Fully extend',
+    user_id: 1
+  },
+  {
+    id: 2,
+    muscle_group: 'Flat Bench',
+    sets: 3,
+    reps: 12,
+    weight: 225,
+    image: "https://cdn5.vectorstock.com/i/1000x1000/49/69/man-doing-cable-tricep-pull-down-exercise-vector-35544969.jpg",
+    notes: 'Straight bar',
+    user_id: 2
+  },
+  {
+      id: 3,
+      muscle_group: 'Bicep Curls',
+      sets: 4,
+      reps: 15,
+      weight: 35,
+      image: "https://cdn5.vectorstock.com/i/1000x1000/49/69/man-doing-cable-tricep-pull-down-exercise-vector-35544969.jpg",
+      notes: 'Fully extend',
+      user_id: 3
+    }
+]
+
+export default mockRoutines
