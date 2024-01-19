0ef17b6ac5315f432c9b2628747238b61bca7ed3
Mikechavo <mchavez76@yahoo.com>
2024-01-19 16:29:59 -0600

    added about app and me modals

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react"
 import { Nav, NavItem } from "reactstrap"
 import { NavLink, useNavigate } from "react-router-dom"
+import { CardGroup, Card, CardBody, CardTitle, CardText, CardImg, Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import './Header.css'
 
 const Header = ({ currentUser, logout }) => {
@@ -18,50 +19,63 @@ const Header = ({ currentUser, logout }) => {
 
   return (
     <Nav className='nav-bar'>
+      <Button>
       <NavItem>
         <NavLink to="/" className="nav-link">
           Home Page
         </NavLink>
       </NavItem>
+      </Button>
+      <Button>
       <NavItem>
         <NavLink to="/routineindex" className="nav-link">
           Example Routines
         </NavLink>
       </NavItem>
+      </Button>
       {/* currentuser exists */}
       {currentUser && (
         <>
+        <Button>
         <NavItem>
           <NavLink to="/myroutines" className="nav-link">
             My Routines
           </NavLink>
         </NavItem>
+        </Button>
+        <Button>
         <NavItem>
         <NavLink to="/routinenew" className="nav-link">
           New Routine
         </NavLink>
       </NavItem>
+      </Button>
+      <Button>
         <NavItem>
         <NavLink to="/" className="nav-link" onClick={signOut}>
           Sign Out
         </NavLink>
       </NavItem>
+      </Button>
       </>
       )}
       {/* currentuser is false aka doesn't exist */}
       {!currentUser && (
         <>
+          <Button>
           <NavItem>
             <NavLink to="/signin" className="nav-link">
               Sign In
             </NavLink>
           </NavItem>
+          </Button>
+          <Button>
           <NavItem>
             <NavLink to="/signup" className="nav-link">
               Sign Up
             </NavLink>
           </NavItem>
-          
+          </Button>
         </>
       )}
     </Nav>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../pages/Home.css'
 import fitHome from '../assets/fitHome.png'
+import me from '../assets/me.jpg'
+import { CardGroup, Card, CardBody, CardTitle, CardText, CardImg, Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
-const Home = () => {
+const Home = (args) => {
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
+
   return (
     <div className='home-container'>
       <div className='image-background'>
@@ -12,6 +18,46 @@ const Home = () => {
       <div className='home-title'>
         <h1>Welcome to FitFolio</h1>
         <img src= {fitHome}/>
+        <CardGroup>
+        <Card>
+          <Button>
+        <CardTitle onClick={toggle}>
+          About FitFolio
+        </CardTitle>
+        </Button>
+        <Modal isOpen={modal} toggle={toggle} {...args}>
+        <ModalHeader toggle={toggle}>About FitFolio</ModalHeader>
+        <ModalBody>
+        Allows you to craft your perfect workout with Fitfolio! Tailor routines to your goals, choose from a variety of exercises, and track progress effortlessly. User-friendly, flexible, and designed for your fitness journey. Stay tuned in for updates as I refine and enhance this fitness journey companion!
+        </ModalBody>
+        <ModalFooter>
+          <Button color="secondary" onClick={toggle}>
+            Close
+          </Button>
+        </ModalFooter>
+      </Modal>
+      </Card>
+
+      <Card>
+          <Button>
+        <CardTitle onClick={toggle}>
+          About Us
+        </CardTitle>
+        </Button>
+        <Modal isOpen={modal} toggle={toggle} {...args}>
+        <ModalHeader toggle={toggle}>Mike Chavez</ModalHeader>
+        <CardImg src= {me}/>
+        <ModalBody>
+        I'm a dedicated US Army Veteran with a proven track record of leadership and responsibility as a Squad Leader. I'm passionate about technology, not only as a profession but as a personal interest. In my free time, I enjoy building, maintaining, and repairing personal computers.
+        </ModalBody>
+        <ModalFooter>
+          <Button color="secondary" onClick={toggle}>
+            Close
+          </Button>
+        </ModalFooter>
+      </Modal>
+      </Card>
+      </CardGroup>
       </div>
     </div>
   )
